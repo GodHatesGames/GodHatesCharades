c8e4b6bd6fa542208c908d660cac858ee865e8a3
Grant Helton <ghelton@gmail.com>
2013-10-16 01:00:26 -0700

    fixed default path

--- js/routes.js
+++ js/routes.js
@@ -11,7 +11,7 @@ define([
 		function($stateProvider, $urlRouterProvider) {
 
 		// For any unmatched url, redirect to /state1
-		$urlRouterProvider.otherwise("/home");
+		$urlRouterProvider.otherwise("/");
 
 		$stateProvider.state('home', {
 			url: "/",
