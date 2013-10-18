c92cc5b2f9171c1f8f045e477b58062d68ae4070
Grant H <ghelton@gmail.com>
2013-10-18 01:12:30 -0700

    added card submission tool, not saving ot server yet

---
+++ components/textarea-noreturn.js
@@ -0,0 +1,25 @@
+define([
+	'angular',
+	'app'
+	], 
+	function(angular, app) {
+		angular.module('textareaNoreturn', [])
+			.directive('textareaNoreturn', [function() { return {
+				restrict: 'A',
+				require: '?ngModel',
+				link: function($scope, $element, attrs, ngModel) {
+					// don't do anything unless this is actually bound to a model
+					if (!ngModel) {
+						return;
+					}
+
+					// view -> model
+					$element.bind('keypress', function(event) {
+						if (event.keyCode == 13) {
+								event.preventDefault();
+							}
+					});
+				}
+			}}]);
+	}
+);
