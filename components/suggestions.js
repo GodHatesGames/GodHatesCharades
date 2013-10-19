c9200df8ec32d54bc6c1826eef250506be6fd5b3
Grant H <ghelton@gmail.com>
2013-10-19 14:09:18 -0700

    0.6

--- components/suggestions.js
+++ components/suggestions.js
@@ -12,18 +12,31 @@ define([
 				replace: true,
 				scope: true,
 				link: function($scope, $element) {
-					var suggestions = new SuggestionService.collection();
-					var suggestionsPromise = suggestions.load();
-					suggestionsPromise.then($scope.onSuggestionsLoaded);
+					// $scope.loadSuggestions()
 				},
 				controller: function($scope, $element) {
-					$scope.suggestions = [];
+					$scope.visibleSuggestions = [];
+					$scope.allSuggestions = {};
+					$scope.suggestionsLoaded = 0;
 					$scope.zeroCount = 0;
 					$scope.oneCount = 0;
+					$scope.filterOptions = {
+						filterText: "",
+						useExternalFilter: true
+					};
+					$scope.pagingOptions = {
+						pageSizes: [1000],
+						pageSize: 1000,
+						currentPage: 1
+					};
 					$scope.gridOptions = {
-						data: 'suggestions',
+						data: 'visibleSuggestions',
+						totalServerItems: 'totalServerItems',
 						enableHighlighting: true,
 						enableRowSelection: false,
+						enablePaging: true,
+						// showFooter: true,
+						pagingOptions: $scope.pagingOptions,
 						columnDefs: [
 						{
 							field: 'attributes.text',
@@ -34,22 +47,86 @@ define([
 							width: 60
 						}]
 					};
+					$scope.totalServerItems = 0;
+					$scope.getSkip = function() {
+						return ($scope.pagingOptions.currentPage - 1) * $scope.pagingOptions.pageSize;
+					}
 
 					$scope.onSuggestionsLoaded = function(suggestions) {
-						var suggestionsArr = suggestions.toArray();
 						var suggestion, type;
-						for(var i = 0; i < suggestionsArr.length; i++) {
-							suggestion = suggestionsArr[i];
-							$scope.suggestions.push(suggestion);
-							type = suggestion.get('type');
-							if(type === 0)
-								$scope.zeroCount++;
-							else if(type === 1)
-								$scope.oneCount++;
+						$scope.visibleSuggestions = [];
+						for(var i = 0; i < suggestions.length; i++) {
+							suggestion = suggestions[i];
+							
+							// save in visible array
+							$scope.visibleSuggestions.push(suggestion);
+
+							// record all loaded suggestions
+							if(!$scope.allSuggestions[suggestion.id]) {
+								$scope.allSuggestions[suggestion.id] = suggestion;
+								type = suggestion.get('type');
+								if(type === 0)
+									$scope.zeroCount++;
+								else if(type === 1)
+									$scope.oneCount++;
+								$scope.suggestionsLoaded++;
+							}
 						}
 
-						console.log('suggestions:', $scope.suggestions);
+						$scope.totalServerItems = $scope.getSkip() + $scope.pagingOptions.pageSize;
+						if($scope.visibleSuggestions.length === $scope.pagingOptions.pageSize)
+							$scope.totalServerItems += $scope.pagingOptions.pageSize;
+						if($scope.totalServerItems < $scope.suggestionsLoaded)
+							$scope.totalServerItems = $scope.suggestionsLoaded;
+
+						if (!$scope.$$phase) {
+							$scope.$apply();
+						} 
 					}
+
+					$scope.loadSuggestions = function (pageSize, page, filter) {
+						if(filter && filter.length > 0) {
+							var filteredList = [];
+							for(var suggestionId in $scope.allSuggestions) {
+								suggestion = $scope.allSuggestions[suggestionId];
+								text = suggestion.get('text').toLowerCase();
+								if(text.indexOf(filter) > -1) {
+									filteredList.push(suggestion);
+								}
+							}
+							$scope.onSuggestionsLoaded(filteredList);
+						} else {
+							var SuggestionObject = Parse.Object.extend("Suggestion");
+							var query = new Parse.Query(SuggestionObject);
+							query.limit(pageSize);
+							query.skip($scope.getSkip());
+							if(filter) {
+								console.log('filter:', filter);
+								query.startsWith('text', filter);
+							}
+							query.find({
+								success: $scope.onSuggestionsLoaded
+							});
+						}
+					};
+						
+						$scope.loadSuggestions($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+					$scope.$watch('pagingOptions', function (newVal, oldVal) {
+						if ((newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) ||
+							newVal.filterUpdated) {
+							console.log('fetch after page options updated');
+							$scope.pagingOptions.filterUpdated = false;
+							$scope.loadSuggestions($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+						}
+					}, true);
+					$scope.$watch('filterOptions', function (newVal, oldVal) {
+						if (newVal !== oldVal) {
+							$scope.pagingOptions.currentPage = 1;
+							$scope.pagingOptions.filterUpdated = true;
+							// $scope.loadSuggestions($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+						}
+					}, true);
 				}
 			}
 		}]);
