72b02b78f42c7ff300a6c5a901c039d7ccf026fd
Grant H <ghelton@gmail.com>
2013-10-21 23:58:09 -0700

    added admin check to main nav, created isAdmin on parseUser

--- components/nav.js
+++ components/nav.js
@@ -4,11 +4,15 @@ define([
 	], 
 	function(angular, app) {
 
-		app.directive('nav', [function() {
+		app.directive('nav', ['parseUser', function(parseUser) {
 			return {
 				restrict: 'E', /* E: Element, C: Class, A: Attribute M: Comment */
 				templateUrl: 'components/nav.html',
-				replace: true
+				replace: true,
+				scope: true,
+				controller: function($scope, $element) {
+					$scope.isAdmin = parseUser.isAdmin;
+				}
 			}
 		}]);
 	}
