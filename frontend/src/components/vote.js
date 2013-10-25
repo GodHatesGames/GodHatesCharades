ce4ee04a5e2cb9ec8c2f4d12a4dcd2773f0dd594
Grant H <ghelton@gmail.com>
2013-10-25 13:45:58 -0700

    progress on voting, now loads new cards as user progresses, still need to add vote call

--- frontend/src/components/vote.js
+++ frontend/src/components/vote.js
@@ -10,18 +10,23 @@ define([
 				templateUrl: 'components/vote.html',
 				replace: true,
 				link: function($scope, $element) {
-
+					$scope.$watch('pairIndex', $scope.onPairIndexChanged);
 				},
 				controller: function($scope, $element) {
+					// public vars
 					$scope.pairLimit = 2;
 					$scope.loading = true;
 					$scope.suggestionPairs = [];
-					$scope.suggestionIndex = 0;
-					loadSuggestionPairs();
+					$scope.suggestionPairSrc = [];
+					$scope.pairIndex = 0;
 
-					function loadSuggestionPairs() {
+					// Private methods
+
+					function loadSuggestionPairs(skip) {
+						$scope.loading = true;
+						
 						Parse.Cloud.run('getRandomSuggestionPairs', {
-							skip: 0
+							skip: skip
 						}, {
 							success: onSuggestionPairsLoaded,
 							error: onSuggestionPairsError
@@ -29,9 +34,9 @@ define([
 					}
 
 					function onSuggestionPairsLoaded(suggestionPairs) {
-						$scope.suggestionPairs = suggestionPairs;
+						$scope.suggestionPairSrc = $scope.suggestionPairSrc.concat(suggestionPairs);
 						$scope.loading = false;
-						$scope.suggestionIndex = 0;
+						updateSuggestionPairs();
 						$scope.$digest();
 					}
 
@@ -39,17 +44,45 @@ define([
 						console.log('couldn\'t find any pairs:', error);
 					}
 
+					function updateSuggestionPairs() {
+						var start = $scope.pairIndex;
+						var end = $scope.pairIndex + $scope.pairLimit;
+						console.log('index range:', start, end - 1);
+						/* slice extracts up to but not including end */
+						var newPairs = $scope.suggestionPairSrc.slice(start, end);
+						if(newPairs.length === $scope.pairLimit)
+							$scope.suggestionPairs = newPairs;
+						 else {
+							console.log('load again');
+							if(!$scope.loading)
+								loadSuggestionPairs($scope.pairIndex);
+						}
+					};
+
+					// Public Methods
+
 					$scope.typeClass = function(suggestion) {
 						var type = suggestion.get('type');
-						if(type === 0)
+						if(type === 0) {
 							return 'character';
-						else
+						} else {
 							return 'scenario';
-					}
+						}
+					};
+
+					$scope.onPairIndexChanged = function(newValue, oldValue) {
+						if($scope.suggestionPairSrc.length > 0) {
+							updateSuggestionPairs();
+						}
+					};
 
 					$scope.selectPair = function(index) {
 						console.log('selected:', index);
-					}
+						$scope.pairIndex += $scope.pairLimit;
+					};
+
+					// init
+					loadSuggestionPairs();
 
 				}
 			}
