cd3f613d006febd8b9a5206bad27d0660eb27a24
Grant H <ghelton@gmail.com>
2013-12-07 18:04:00 -0800

    updated homepage content to add timer and subscription thing

---
+++ frontend/src/js/campaignService.js
@@ -0,0 +1,24 @@
+define([
+	'app'
+	], 
+	function(app) {
+
+		app.service('campaignService', [function() {
+			var campaignService = {
+				campaignsById: {
+					ks2013: {
+						done: function() {
+							return Boolean(this.deadline() <= 0);
+						},
+						deadline: function getDeadline() {
+							// 1388534400000: Jan 1 2014 GMT
+							return (1386467773000 - new Date().getTime());
+						}
+					}
+				}
+			}
+
+			return campaignService;
+		}]);
+	}
+);
