c0fae3e810fa6948cf9aa54b2b4f6f47b805e648
Grant H <ghelton@gmail.com>
2013-12-27 11:47:15 -0800

    double quotes to single

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -12,26 +12,26 @@ define([
 				 $urlRouterProvider) {
 
 		// For any unmatched url, redirect to /state1
-		$urlRouterProvider.otherwise("/");
+		$urlRouterProvider.otherwise('/');
 
 		$stateProvider.state('home', {
-			url: "/",
-			templateUrl: "views/homeView.html",
+			url: '/',
+			templateUrl: 'views/homeView.html',
 			controller: function($scope, campaignService) {
 				$scope.kickstarter = campaignService.campaignsById['ks2013'];
 			}
 		});
 		$stateProvider.state('submit', {
-			url: "/submit",
-			templateUrl: "views/submitView.html"
+			url: '/submit',
+			templateUrl: 'views/submitView.html'
 		});
 		$stateProvider.state('vote', {
-			url: "/vote",
-			templateUrl: "views/voteView.html"
+			url: '/vote',
+			templateUrl: 'views/voteView.html'
 		});
 		$stateProvider.state('login', {
-			url: "/login",
-			templateUrl: "views/loginView.html",
+			url: '/login',
+			templateUrl: 'views/loginView.html',
 			resolve: {
 				parseUser: 'parseUser'
 			},
@@ -40,15 +40,15 @@ define([
 			}
 		});
 		$stateProvider.state('user', {
-			url: "/user/:userid",
-			templateUrl: "views/publicProfileView.html",
+			url: '/user/:userid',
+			templateUrl: 'views/publicProfileView.html',
 			controller: function ($scope, $stateParams) {
 				$scope.userid = $stateParams.userid;
 			}
 		});
 		$stateProvider.state('card', {
-			url: "/card/:cardid",
-			templateUrl: "views/cardView.html",
+			url: '/card/:cardid',
+			templateUrl: 'views/cardView.html',
 			resolve: {
 				bitly: function($stateParams, $location, $q, $timeout, $rootScope) {
 					var voteId = $stateParams.voteid;
@@ -64,7 +64,7 @@ define([
 						type: 'GET',
 						url: 'https://api-ssl.bitly.com/v3/shorten',
 						async: false,
-						contentType: "application/json",
+						contentType: 'application/json',
 						dataType: 'jsonp',
 						data: params
 					}).then(function(data) {
@@ -111,20 +111,20 @@ define([
 			}
 		});
 		$stateProvider.state('suggestions', {
-			url: "/suggestions",
-			templateUrl: "views/suggestionsView.html"
+			url: '/suggestions',
+			templateUrl: 'views/suggestionsView.html'
 		});
 		$stateProvider.state('rules', {
-			url: "/rules",
-			templateUrl: "views/rulesView.html"
+			url: '/rules',
+			templateUrl: 'views/rulesView.html'
 		});
 		$stateProvider.state('moderation', {
-			url: "/moderation",
-			templateUrl: "views/moderationView.html"
+			url: '/moderation',
+			templateUrl: 'views/moderationView.html'
 		});
 		$stateProvider.state('top', {
-			url: "/top",
-			templateUrl: "views/topView.html"
+			url: '/top',
+			templateUrl: 'views/topView.html'
 		});
 	}]);
 
