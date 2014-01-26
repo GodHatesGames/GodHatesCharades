ae314b0290f6c53500a701c89757b34d2fe96d94
Grant H <ghelton@gmail.com>
2014-01-26 13:59:19 -0800

    removed unused randstring method, added listener for statechangeerror, cleaned up some jshint issues

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,9 @@
+'use strict';
 // Parse Init
 Parse.initialize(CONFIG.PARSE_APP_ID, CONFIG.PARSE_JS_KEY);
 
 // Angular Init
-var app = angular.module('app', ['ng', 
+var app = angular.module('app', ['ng',
 									'ui.router',
 									'ui.bootstrap',
 									'infinite-scroll',
@@ -20,8 +21,8 @@ app.config(function($locationProvider){
 	}
 );
 
-app.run(function($rootScope,   
-						$state,   
+app.run(function($rootScope,
+						$state,
 						$stateParams,
 						parseUser
 			) {
@@ -32,14 +33,6 @@ app.run(function($rootScope,
 
 				// Default away value
 				$rootScope.isAway = false;
-
-				function randString() {
-					// copy pasta'd from: http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
-					return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-						var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-						return v.toString(16);
-					});
-				}
 
 				$rootScope.$state = $state;
 				$rootScope.$stateParams = $stateParams;
@@ -56,16 +49,19 @@ app.run(function($rootScope,
 
 				$rootScope.$on('$stateChangeStart', function(ev, to, toParams, from, fromParams) {
 					$rootScope.setLoading();
-					$rootScope.contentLoaded = false;
 					if(to.name === 'home')
 						$rootScope.isAway = false;
 					else
 						$rootScope.isAway = true;
-				})
+				});
 
 				$rootScope.$on('$stateChangeSuccess', function (ev, to, toParams, from, fromParams) {
 					$rootScope.unsetLoading();
-					$rootScope.contentLoaded = true;
+				});
+
+				$rootScope.$on('$stateChangeError', function (ev, to, toParams, from, fromParams, error) {
+					$rootScope.unsetLoading();
+					console.log('WHOOAAAATTT!!! ' + error);
 				});
 			}
 );
