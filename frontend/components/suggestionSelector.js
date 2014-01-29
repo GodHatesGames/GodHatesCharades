990796d7f5d501f5d921ee10bcdc71d2de11b720
Grant H <ghelton@gmail.com>
2014-01-28 23:58:34 -0800

    added search to set builder

--- frontend/components/suggestionSelector.js
+++ frontend/components/suggestionSelector.js
@@ -66,3 +66,31 @@ app.directive('suggestionSelector', function(cardService, $filter, $state) {
 		}
 	};
 });
+
+app.filter('property', function() {
+
+	function parseString(input){
+		return input.split('.');
+	}
+
+	function getValue(element, propertyArray){
+		var value = element;
+
+		_.forEach(propertyArray, function(property){
+			value = value[property];
+		});
+
+		return value;
+	}
+
+	return function (array, propertyString, target){
+		if(!target)
+			return array;
+		
+		var properties = parseString(propertyString);
+
+		return _.filter(array, function(item){
+			return getValue(item, properties).toLowerCase().indexOf(target) > -1;
+		});
+	};
+});
