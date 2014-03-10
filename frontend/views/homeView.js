87a2948f7706f4e2e7e8e54ae2d81cde255b1b83
Grant H <ghelton@gmail.com>
2014-03-10 16:57:01 -0700

    added left/right hands for slides

--- frontend/views/homeView.js
+++ frontend/views/homeView.js
@@ -2,4 +2,27 @@
 app.controller('homeView', function($scope, campaignService) {
 	$scope.kickstarter = campaignService.campaignsById.ks2013;
 	$scope.ipad = campaignService.campaignsById.iPad2014;
+
+	$scope.currentSlide = 0;
+	$scope.maxSlides = 3;
+	$scope.getSlideForIndex = function(index) {
+		if(index === $scope.currentSlide) {
+			return 'curr';
+		} else if(index < $scope.currentSlide) {
+			return 'prev';
+		} else {
+			return 'next';
+		}
+	};
+
+	$scope.prevSlide = function() {
+		if ($scope.currentSlide > 0)
+			$scope.currentSlide--;
+	};
+
+	$scope.nextSlide = function() {
+		if ($scope.currentSlide < $scope.maxSlides)
+			$scope.currentSlide++;
+	};
+
 });
