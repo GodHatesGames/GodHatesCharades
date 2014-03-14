f020e510f319dc3453270b58d23d42229867c15f
Grant H <ghelton@gmail.com>
2014-03-13 23:13:04 -0700

    added printer set id

--- frontend/js/config_Local_Dist.js
+++ frontend/js/config_Local_Dist.js
@@ -1,8 +1,9 @@
 var CONFIG = {
 	// This app uses parse to access/save player creations 
-	PARSE_APP_ID: 'GvWVZYlfePPR74oSKdDgftmBKzj3lsprcNlgiy4Z',
-	PARSE_JS_KEY: 'gQeODbEWxHlZVulVVT5EKwP8wRXI4SvX2tlcaCyV',
+	PARSE_APP_ID: 'awD3vEIkjdxeLpMVrwv0bD8JugIgB3gzbkCMFQkt',
+	PARSE_JS_KEY: 'S11gCWChxmk9Wmfua9HmIiIVEKxaIW63VD90oINh',
 	APP_VERSION: '0.0.3',
-	EXAMPLE_SET_ID: 'm0zujLxAuH',
+	EXAMPLE_SET_ID: 'JkiS8vM1XW',
+	PRINT_SET_ID: 'JkiS8vM1XW',
 	APP_PLATFORM: 'site_dist'
 };
--- frontend/views/exportView.js
+++ frontend/views/exportView.js
@@ -1,7 +1,7 @@
 'use strict';
 app.controller('exportView', function(sets, $scope, $filter) {
-	sets.getCardsForSet(sets.byId[CONFIG.EXAMPLE_SET_ID])
+	sets.getCardsForSet(sets.byId[CONFIG.PRINT_SET_ID])
 	.then(function(setItems) {
-		$scope.setItems = $filter('orderBy')(setItems, '-attributes.type');
+		$scope.setItems = setItems;
 	});
 });
