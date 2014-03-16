b1b06be5b6af2d2db8896704f07c28b3d2a298a2
Grant H <ghelton@gmail.com>
2014-03-16 13:38:23 -0700

    improved export styling, better feedback for how many suggestions loaded

--- frontend/views/exportView.js
+++ frontend/views/exportView.js
@@ -1,6 +1,7 @@
 'use strict';
 app.controller('exportView', function(getAllSuggestions, sets, $scope, $filter) {
 	$scope.allSuggestions = getAllSuggestions.data;
+	$scope.setItems = [];
 	sets.getCardsForSet(sets.byId[CONFIG.PRINT_SET_ID])
 	.then(function(setItems) {
 		$scope.setItems = setItems;
