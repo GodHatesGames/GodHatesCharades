00b4260935a6e54077b7974ac9106a4e4d0df6bb
Grant H <ghelton@gmail.com>
2014-05-07 00:32:07 -0700

    rendering html as a view from express to allow custom paths

--- api/server.js
+++ api/server.js
@@ -5,18 +5,18 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var files = process.env.FILES || '../frontend';
 
-var server = express(); // better instead
-// server.use(express.logger());
-server.use(express.bodyParser());
-
 // Define our static file directory, it will be 'public'
 var staticFilePath = path.join(__dirname, files);
 console.log('staticFilePath:', staticFilePath);
-server.use('/', express.static(staticFilePath));
 
-server.get('/[^.]+$', function(req, res){
-	res.set('Content-Type', 'text/html')
-	.sendfile(staticFilePath + '/index.html');
+var server = express(); // better instead
+server.set('views', staticFilePath);
+server.use(express.logger());
+server.use(express.bodyParser());
+
+server.use(express.static(staticFilePath));
+server.get('/*', function(req, res) {
+	res.render('index');
 });
 
 
