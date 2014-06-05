779d4f5f4822dc240ea35affb36e683a6831c80d
Grant H <ghelton@gmail.com>
2014-06-04 21:49:21 -0700

    separated youtube html

--- frontend/components/youtube.js
+++ frontend/components/youtube.js
@@ -3,7 +3,7 @@ app.directive('youtube', function() {
 	return {
 		restrict: 'E',
 		replace: true,
-		template: '<iframe-fluid url="videoUrl"></iframe-fluid>',
+		templateUrl: 'components/youtube.html',
 		link: function($scope, $element, $attr){
 			$attr.$observe('id', function(newValue) {
 				$scope.videoUrl = '//www.youtube.com/embed/' + $scope.$eval(newValue);
