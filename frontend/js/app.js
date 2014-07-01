70b2a58a61a7b9957bd37c8e44051185ecf1165d
Grant H <ghelton@gmail.com>
2014-06-30 22:31:17 -0700

    tabs

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,20 +5,20 @@ Parse.initialize(CONFIG.PARSE_APP_ID, CONFIG.PARSE_JS_KEY);
 
 // Angular Init
 var app = angular.module('app', ['ng',
-									'ngAnimate',
-									'ui.router',
-									'ui.bootstrap',
-									'textareaNoreturn',
-									'parse.user',
-									'ngSanitize',
-									'restangular',
-									'ngHtmlCompile',
-									'ngContent',
-									'prismic.io',
-									'slugifier',
-									'disqusHere',
-									'ngCsv'
-								]);
+                                 'ngAnimate',
+                                 'ui.router',
+                                 'ui.bootstrap',
+                                 'textareaNoreturn',
+                                 'parse.user',
+                                 'ngSanitize',
+                                 'restangular',
+                                 'ngHtmlCompile',
+                                 'ngContent',
+                                 'prismic.io',
+                                 'slugifier',
+                                 'disqusHere',
+                                 'ngCsv'
+                                ]);
 console.log('starting app');
 
 app.config(function($locationProvider, PrismicProvider){
@@ -28,9 +28,9 @@ app.config(function($locationProvider, PrismicProvider){
 		if (CONFIG.PRISMIC) {
 			PrismicProvider.setApiEndpoint(CONFIG.PRISMIC.API_ENDPOINT);
 		}
-	    PrismicProvider.setLinkResolver(function(ctx, doc) {
-	        return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
-	    });
+		PrismicProvider.setLinkResolver(function(ctx, doc) {
+			return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
+		});
 	}
 );
 
