5f59e3bad40b09763d484f0aa9a91803698a1eb1
Grant H <ghelton@gmail.com>
2014-07-08 17:37:07 -0700

    added missing analytics for signup form

--- frontend/views/voteView.js
+++ frontend/views/voteView.js
@@ -1,3 +1,10 @@
 app.controller('voteView', function($scope, leanplum) {
 	$scope.leanplum = leanplum;
+
+	$scope.onEmailSubmitted = function(fromLabel) {
+		$scope.emailSubmitted = true;
+		Leanplum.track('Signed up for email updates');
+		ga('send', 'event', 'signup', 'newsletter', fromLabel);
+		_pa.track('signup_newletter');
+	};
 });
