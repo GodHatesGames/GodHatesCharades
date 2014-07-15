c973856342dc577e95fc606f1cfef2f17c741815
Grant H <ghelton@gmail.com>
2014-07-15 13:51:53 -0700

    added missing key for parse version

--- frontend/js/config_Prod.js
+++ frontend/js/config_Prod.js
@@ -2,6 +2,7 @@ var CONFIG = {
 	// This app uses parse to access/save player creations 
 	PARSE_APP_ID: 'awD3vEIkjdxeLpMVrwv0bD8JugIgB3gzbkCMFQkt',
 	PARSE_JS_KEY: 'S11gCWChxmk9Wmfua9HmIiIVEKxaIW63VD90oINh',
+	PARSE_VERSION: 'v2_',
 	APP_VERSION: '0.0.3',
 	EXAMPLE_SET_ID: 'JkiS8vM1XW',
 	PRINT_SET_ID: 'wy2krlPFGt',
