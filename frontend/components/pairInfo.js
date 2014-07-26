4067d68f2fd12a029e930a90280568835a920793
Grant H <ghelton@gmail.com>
2014-07-26 15:25:35 -0700

    fixed pair info sentence

--- frontend/components/pairInfo.js
+++ frontend/components/pairInfo.js
@@ -7,22 +7,12 @@ app.directive('pairInfo', function(parseUser, pairService, $rootScope) {
 			id: '=id'
 		},
 		controller: function($scope, $element) {
-			$scope.alwaysShow = {'0': 'Nobody has seen this pair...',
-			                     'one': 'This pair was voted on once,',
-			                     'other': 'This pair was voted on {{pair.attributes.displayed}} times,'
-			                    };
+			$scope.times = {'0': 'times',
+			                'one': 'time',
+			                'other': 'times'};
+			$scope.person = {'one': 'person',
+		                     'other': 'people'};
 
-			$scope.halfsies = {'other': 'and half thought it was funny, the other half thought it wasn\'t.'
-			                   };
-
-			$scope.onlyFunny = {'one': 'and they thought it was funny.',
-			                    'other': 'and they all thought it was funny.'
-			                   };
-
-			$scope.mixedBag = {'0': 'nobody thought it was funny.',
-			                    'one': 'and they thought it was funny.',
-			                    'other': '{{pair.attributes.chosen}} people thought it was funny.'
-			                   };
 			$scope.$watch('id', onIdChanged);
 
 			function onIdChanged(newValue) {
