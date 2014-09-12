1e7d754934f32c6cc48720c241cd3d30394d7500
Grant H <ghelton@gmail.com>
2014-09-11 20:52:52 -0700

    added error message for reset

--- frontend/views/resetPasswordView.js
+++ frontend/views/resetPasswordView.js
@@ -4,15 +4,16 @@ app.controller('resetPasswordView', function(parseUser, $scope) {
 	$scope.reset = _reset;
 
 	function _reset() {
+		$scope.message = '';
 		parseUser.resetPassword($scope.email)
 		.then(_onResetSent, _onResetFail);
 	};
 
 	function _onResetSent() {
-		$scope.message = "Awesome, check your email for the reset link!";
+		$scope.message = 'Awesome, check your email for the reset link!';
 	}
 
 	function _onResetFail(error) {
-		$scope.message = "Oops something went wrong, our server-bot says: ", error.message;
+		$scope.message = 'Oops something went wrong, our server-bot says: ' + error.message;
 	}
 });
