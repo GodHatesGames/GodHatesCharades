6adf4e30508d7a54ab5619773d21272a4df81165
Grant H <ghelton@gmail.com>
2014-12-27 11:17:40 -0800

    updated saving feedback

--- frontend/components/setSelector.js
+++ frontend/components/setSelector.js
@@ -11,22 +11,36 @@ app.directive('setSelector', function(parseUser, pairService, $rootScope, sets) 
 			$scope.sets = sets;
 			$scope.onSelect = _onSelect;
 			$scope.onRemove = _onRemove;
-			updateModel();
+			$scope.saving = false;
+			$scope.feedbackClass = _feedbackClass;
+			_updateModel();
 
-			function updateModel() {
+			function _updateModel() {
 				var currentSetIds = sets.setIdsByCardId[$scope.suggestion.id];
 				var currentSets = _.pick(sets.byId, currentSetIds);
 				$scope.selectedSets = _.values(currentSets);
 			}
 
 			function _onSelect(set) {
+				$scope.saving = true;
 				sets.addCardToSet($scope.suggestion, set)
-				.then(updateModel);
+				.then(_updateModel)
+				.then(_onComplete);
 			}
 
 			function _onRemove(set) {
+				$scope.saving = true;
 				sets.removeCardFromSet($scope.suggestion, set)
-				.then(updateModel);
+				.then(_updateModel)
+				.then(_onComplete);
+			}
+
+			function _onComplete() {
+				$scope.saving = false;
+			}
+
+			function _feedbackClass() {
+				return $scope.saving ? 'glyphicon-refresh custom-spin' : 'glyphicon-ok-circle';
 			}
 		}
 	}
