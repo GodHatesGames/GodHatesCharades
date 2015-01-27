3c85bd7ed1c629ed2e9bce84b78d51144ff89665
Grant H <ghelton@gmail.com>
2015-01-26 19:14:08 -0800

    fixed event tracking

--- frontend/views/homeView.js
+++ frontend/views/homeView.js
@@ -12,6 +12,7 @@ app.controller('homeView', function($scope, $window, $timeout) {
 		rel: 0
 	};
 	$scope.preorderLink = 'http://godhatesgames.myshopify.com/cart/1051755037:1?source_app=shopify-widget?referer=https%3A%2F%2Fgodhatescharades.com';
+	$scope.onPreorderClicked = _onPreorderClicked;
 
 	angular.element($window).bind('scroll', function() {
 		if (this.pageYOffset >= 100) {
