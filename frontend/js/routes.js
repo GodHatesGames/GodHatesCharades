f4e36bac564f9f348f3604dbfb7619b1718b2ed3
Grant H <ghelton@gmail.com>
2015-02-18 15:09:03 -0800

    commented watch page to avoid errors

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -325,26 +325,26 @@ app.config(function($stateProvider,
 		}
 	});
 
-	$stateProvider.state('watch', {
-		url: '/watch',
-		title: 'Watch',
-		description: 'Watch people make fools out of themselves on the internet!',
-		templateUrl: 'views/watchView.html',
-		controller: 'watchView',
-		resolve: {
-			readyForUpload: ['ytUploadService', function(ytUploadService) {
-				return ytUploadService.waitForLoad();
-			}],
-			ghcVids:['Restangular', function(Restangular) {
-				var params = {
-					part: 'snippet',
-					key: CONFIG.YOUTUBE.key,
-					q: 'I just acted out a scene from God Hates Charades',
-					type: 'video',
-					order: 'date'
-				}
-				return Restangular.oneUrl('ghcVids', 'https://www.googleapis.com/youtube/v3/').one('search').get(params);
-			}]
-		}
-	});
+	// $stateProvider.state('watch', {
+	// 	url: '/watch',
+	// 	title: 'Watch',
+	// 	description: 'Watch people make fools out of themselves on the internet!',
+	// 	templateUrl: 'views/watchView.html',
+	// 	controller: 'watchView',
+	// 	resolve: {
+	// 		readyForUpload: ['ytUploadService', function(ytUploadService) {
+	// 			return ytUploadService.waitForLoad();
+	// 		}],
+	// 		ghcVids:['Restangular', function(Restangular) {
+	// 			var params = {
+	// 				part: 'snippet',
+	// 				key: CONFIG.YOUTUBE.key,
+	// 				q: 'I just acted out a scene from God Hates Charades',
+	// 				type: 'video',
+	// 				order: 'date'
+	// 			}
+	// 			return Restangular.oneUrl('ghcVids', 'https://www.googleapis.com/youtube/v3/').one('search').get(params);
+	// 		}]
+	// 	}
+	// });
 });
