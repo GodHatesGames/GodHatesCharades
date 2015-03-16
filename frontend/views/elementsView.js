3caa2b05c61ead6e0f76d18aa6f322a616359308
Grant H <ghelton@gmail.com>
2015-03-15 21:18:19 -0700

    added documentation for signup_section

--- frontend/views/elementsView.js
+++ frontend/views/elementsView.js
@@ -3,7 +3,17 @@ app.controller('elementsView', function($scope) {
   $scope.sections = [
     {
       title: 'Signup Section',
-      code: '<signup-section location="homepage_signup_section" class="bg-dark"></signup-section>'
+      code: '<signup-section location="signup_section" class="bg-dark"></signup-section>',
+      props: [
+        {
+          title: 'location',
+          description: 'used in analytics to describe where in a given page the signup happened, unless there are multiple on the page use signup_section.'
+        },
+        {
+          title: 'class',
+          description: 'standard html attribute, for this component use to add a background color. backgrounds listed on the <a ui-sref="admin.components">Components</a> page.'
+        }
+      ]
     }
   ];
 });
