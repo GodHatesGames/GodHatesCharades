e095f172042fcc77348dac80c1bbf980a8f46051
Grant H <ghelton@gmail.com>
2015-04-17 11:57:17 -0700

    restructured watch view

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -317,27 +317,4 @@ app.config(function($stateProvider,
 			}]
 		}
 	});
-
-	$stateProvider.state('watcher', {
-		url: '/watch',
-		title: 'Watch',
-		description: 'Watch people make fools out of themselves on the internet!',
-		templateUrl: 'views/watchView.html',
-		controller: 'watchView',
-		resolve: {
-			readyForUpload: ['ytUploadService', function(ytUploadService) {
-				return ytUploadService.waitForLoad();
-			}],
-			ghcVids:['Restangular', function(Restangular) {
-				var params = {
-					part: 'snippet',
-					key: CONFIG.YOUTUBE.key,
-					q: '"God Hates Charades"',
-					type: 'video',
-					order: 'date'
-				}
-				return Restangular.oneUrl('ghcVids', 'https://www.googleapis.com/youtube/v3/').one('search').get(params);
-			}]
-		}
-	});
 });
