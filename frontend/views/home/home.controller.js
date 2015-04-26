8509b44c527cf4a943bf4af423efd397ef6c81f9
Grant H <ghelton@gmail.com>
2015-04-25 20:45:37 -0700

    Store button should just go to the store; Change preorder events to buy now events;  Change Preorder button to Buy Now

--- frontend/views/home/home.controller.js
+++ frontend/views/home/home.controller.js
@@ -13,8 +13,8 @@ app.controller('homeView', function($scope, $window, $timeout, analytics) {
 		autohide: 1,
 		rel: 0
 	};
-	$scope.preorderLink = 'http://godhatesgames.myshopify.com/cart/1051755037:1?source_app=shopify-widget?referer=https%3A%2F%2Fgodhatescharades.com';
-	$scope.onPreorderClicked = _onPreorderClicked;
+	$scope.buyLink = 'http://godhatesgames.myshopify.com/cart/1051755037:1?source_app=shopify-widget?referer=https%3A%2F%2Fgodhatescharades.com';
+	$scope.onBuyClicked = _onBuyClicked;
 	// $scope.$watch('homeVideo.setVolume', _onHomeVideo);
 	// $scope.$on('youtube.player.ready', _onPlayerReady);
 
@@ -44,7 +44,7 @@ app.controller('homeView', function($scope, $window, $timeout, analytics) {
 		$scope.homeVideo.setVolume(originalVolume);
 	}
 
-	function _onPreorderClicked(location, button) {
+	function _onBuyClicked(location, button) {
 		analytics.mpEvent('Click', {
 			'Location': location,
 			'Button': button
