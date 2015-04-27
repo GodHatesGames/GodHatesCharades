fa611f30c017eca07224fca177bc413c5ae3fac1
Grant H <ghelton@gmail.com>
2015-04-27 15:41:30 -0700

    progress on store layout

--- frontend/views/store/store.controller.js
+++ frontend/views/store/store.controller.js
@@ -1,4 +1,10 @@
 'use strict';
 app.controller('storeView', function(collection, products, $scope) {
   $scope.collection = collection;
+  $scope.cart = {};
+  $scope.buy = _buy;
+
+  function _buy() {
+    
+  }
 });
