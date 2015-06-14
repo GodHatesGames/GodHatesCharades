a35e0508b3e728648ecf91803be418bd5a2e0ee9
Grant H <ghelton@gmail.com>
2015-06-13 17:56:18 -0700

    improved store styling

--- frontend/components/storeProduct.js
+++ frontend/components/storeProduct.js
@@ -1,22 +1,31 @@
 app.directive('storeProduct', function() {
-	return {
-		templateUrl: 'components/storeProduct.html',
-		replace: true,
-		scope: {
-			storeItem: '=',
-			increment: '=',
-			decrement: '=',
-			setQuantity: '=',
-			quantity: '@'
-		},
-		controller: function($scope, $element) {
-			$scope.product = $scope.storeItem.product;
-			$scope.currentImage = $scope.product.images[0];
-			$scope.selectImage = _selectImage;
+  return {
+    templateUrl: 'components/storeProduct.html',
+    replace: true,
+    scope: {
+      storeItem: '=',
+      increment: '=',
+      decrement: '=',
+      setQuantity: '=',
+      quantity: '@'
+    },
+    controller: function($scope, $element) {
+      $scope.product = $scope.storeItem.product;
+      $scope.currentImage = $scope.product.images[0];
+      $scope.selectImage = _selectImage;
+      $scope.productQuantity = _productQuantityGetterSetter;
 
-			function _selectImage (newImage) {
-				$scope.currentImage = newImage;
-			}
-		}
-	}
+      function _selectImage (newImage) {
+        $scope.currentImage = newImage;
+      }
+
+      function _productQuantityGetterSetter(newQuantity) {
+        if(angular.isDefined(newQuantity)) {
+          $scope.setQuantity(newQuantity);
+        }
+
+        return $scope.quantity;
+      }
+    }
+  }
 });
